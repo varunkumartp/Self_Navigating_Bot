5d896eaa326724d31ccad75d7598a4a19b27007b
Varun Kumar T P <varunkumartp@gmail.com>
2021-06-26 18:18:13 +0530

    Added script to clone packages

---
+++ packages.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git clone https://github.com/ros/ros_comm.git
+git clone https://github.com/ros/std_msgs.git
+git clone https://github.com/ros-drivers/rosserial.git
+git clone https://github.com/ros-planning/navigation.git
+git clone https://github.com/ros-drivers/rgbd_launch.git
+git clone https://github.com/ros-drivers/freenect_stack.git
+git clone https://github.com/ros/robot_state_publisher.git
+git clone https://github.com/ros-perception/depthimage_to_laserscan.git
