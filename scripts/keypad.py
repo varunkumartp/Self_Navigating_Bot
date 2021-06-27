cff70b748fdc325f34dae11013a113a5f110c55a
Varun Kumar T P <varunkumartp@gmail.com>
2021-06-27 20:09:49 +0530

    Added three chances to enter correct otp

--- scripts/keypad.py
+++ scripts/keypad.py
@@ -19,20 +19,23 @@ class keypad:
         
         # GPIO
         # row pins
-        self.L1 = 5
-        self.L2 = 6
-        self.L3 = 13
-        self.L4 = 19
+        self.L1 = 6
+        self.L2 = 13
+        self.L3 = 19
+        self.L4 = 26
 
         # column pins
-        self.C1 = 12
-        self.C2 = 16
-        self.C3 = 20
+        self.C1 = 16
+        self.C2 = 20
+        self.C3 = 21
         
+        self.led = 12
+        self.buzzer = 5
+        self.counter = 0
         self.keypadPressed = -1
         self.input = ''
         self.otp = '1234'
-        
+        self.lock = False
         # Setup GPIO
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
@@ -41,6 +44,8 @@ class keypad:
         GPIO.setup(self.L2, GPIO.OUT)
         GPIO.setup(self.L3, GPIO.OUT)
         GPIO.setup(self.L4, GPIO.OUT)
+        GPIO.setup(self.led, GPIO.OUT)
+        GPIO.setup(self.buzzer, GPIO.OUT)
         # Use the internal pull-down resistors
         GPIO.setup(self.C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -91,15 +96,45 @@ class keypad:
         # check if # is pressed 
         GPIO.output(self.L4, GPIO.HIGH)
         if (not pressed and GPIO.input(self.C3) == 1):
-            self.statPub.publish(self.input == self.otp)
+            if self.input == self.otp:
+                self.statPub.publish(True)
+                self.gpio_on('led')
+            else:
+                self.gpio_on('buzzer')
+                self.counter += 1
             pressed = True
         GPIO.output(self.L4, GPIO.LOW)
-
+        
+        if self.counter == 3:
+            self.statPub.publish(False)
+            self.gpio_on('lock')
+                    
         if pressed:
             self.input = ""
 
         return pressed
-
+    
+    #############################################################################
+    def gpio_on(self, pin):
+    #############################################################################
+        if pin == 'led':
+            GPIO.output(self.led, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(self.led, GPIO.LOW)
+        elif pin == 'buzzer':
+            GPIO.output(self.buzzer, GPIO.HIGH)
+            time.sleep(0.2)
+            GPIO.output(self.buzzer, GPIO.LOW)
+            time.sleep(0.2)
+            GPIO.output(self.buzzer, GPIO.HIGH)
+            time.sleep(0.2)
+            GPIO.output(self.buzzer, GPIO.LOW)
+        else:
+            GPIO.output(self.buzzer, GPIO.HIGH)
+            time.sleep(3)
+            GPIO.output(self.buzzer, GPIO.LOW)
+            self.lock = True
+            
     #############################################################################
     def readLine(self, line, characters):
     #############################################################################
@@ -118,6 +153,9 @@ class keypad:
         r = rospy.Rate(self.rate)
         while not rospy.is_shutdown():
             self.check()
+            while self.lock:
+                
+                r.sleep()
             r.sleep()
             
     #############################################################################
@@ -139,6 +177,7 @@ class keypad:
                 self.readLine(self.L3, ["7","8","9"])
                 self.readLine(self.L4, ["*","0","#"])
             time.sleep(0.1)
+            
             
 #############################################################################
 #############################################################################                
