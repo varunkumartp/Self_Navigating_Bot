8a0e358f7247cd2198a8ba79588836967391b69d
Varun Kumar T P <varunkumartp@gmail.com>
2021-06-27 23:23:07 +0530

    Publishes Range messages

--- scripts/ultrasound.py
+++ scripts/ultrasound.py
@@ -15,26 +15,6 @@ class ultrasound:
         self.nodename = rospy.get_name()
         rospy.loginfo("-I- %s started" % self.nodename)
         
-        # ultrasound 
-        self.left_sensor = Range()
-        self.left_sensor.header.frame_id = "/ultrasound_left"
-        self.left_sensor.radiation_type = 0
-        self.left_sensor.field_of_view = 0.523599
-        self.left_sensor.min_range = 0.02
-        self.left_sensor.max_range = 1.00
-        self.right_sensor = Range()
-        self.right_sensor.header.frame_id = "/ultrasound_right"
-        self.right_sensor.radiation_type = 0
-        self.right_sensor.field_of_view = 0.523599
-        self.right_sensor.min_range = 0.02
-        self.right_sensor.max_range = 1.00
-        self.center_sensor = Range()
-        self.center_sensor.header.frame_id = "/ultrasound_center"
-        self.center_sensor.radiation_type = 0
-        self.center_sensor.field_of_view = 0.523599
-        self.center_sensor.min_range = 0.02
-        self.center_sensor.max_range = 1.00
-        
         # subscribers
         rospy.Subscriber("left", Float32, self.leftCallback)
         rospy.Subscriber("center", Float32, self.centerCallback)
@@ -54,23 +34,41 @@ class ultrasound:
     #############################################################################
     def leftCallback(self,msg):
     #############################################################################
-        self.left_sensor.header.stamp = rospy.Time.now()
-        self.left_sensor.range = msg.data
-        self.left.publish(self.left)        
+        left_sensor = Range()
+        left_sensor.header.stamp = rospy.Time.now()
+        left_sensor.header.frame_id = "/ultrasound_left"
+        left_sensor.radiation_type = 0
+        left_sensor.field_of_view = 0.523599
+        left_sensor.min_range = 0.02
+        left_sensor.max_range = 1.00
+        left_sensor.range = msg.data
+        self.left.publish(left_sensor)        
     
     #############################################################################
     def rightCallback(self,msg):
     #############################################################################
-        self.right_sensor.header.stamp = rospy.Time.now()
-        self.right_sensor.range = msg.data
-        self.right.publish(self.right)
+        right_sensor = Range()
+        right_sensor.header.stamp = rospy.Time.now()
+        right_sensor.header.frame_id = "/ultrasound_right"
+        right_sensor.radiation_type = 0
+        right_sensor.field_of_view = 0.523599
+        right_sensor.min_range = 0.02
+        right_sensor.max_range = 1.00
+        right_sensor.range = msg.data
+        self.right.publish(right_sensor)
     
     #############################################################################
     def centerCallback(self,msg):
     #############################################################################
-        self.center_sensor.header.stamp = rospy.Time.now()
-        self.center_sensor.range = msg.data
-        self.center.publish(self.center)
+        center_sensor = Range()
+        center_sensor.header.stamp = rospy.Time.now()
+        center_sensor.header.frame_id = "/ultrasound_center"
+        center_sensor.radiation_type = 0
+        center_sensor.field_of_view = 0.523599
+        center_sensor.min_range = 0.02
+        center_sensor.max_range = 1.00
+        center_sensor.range = msg.data
+        self.center.publish(center_sensor)
         
 #############################################################################
 #############################################################################
