e1acaec2201417eb04439ab11c1e1ea484918d91
Varun Kumar T P <varunkumartp@gmail.com>
2021-06-28 20:58:49 +0530

    added transform broadcaster for sensors

--- scripts/ultrasound.py
+++ scripts/ultrasound.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import Float32
 from sensor_msgs.msg import Range
+from tf.broadcaster import TransformBroadcaster
 
 #############################################################################
 class ultrasound:
@@ -34,39 +35,57 @@ class ultrasound:
     #############################################################################
     def leftCallback(self,msg):
     #############################################################################
+        usl = TransformBroadcaster()
+        usl.sendTransform((0.05 ,0.085, -0.025),
+                          (0, 0, 0.258819, 0.9659258),
+                          rospy.Time.now(),
+                          "ultrasound_left",
+                          "base_link")
         left_sensor = Range()
         left_sensor.header.stamp = rospy.Time.now()
-        left_sensor.header.frame_id = "/ultrasound_left"
+        left_sensor.header.frame_id = "ultrasound_left"
         left_sensor.radiation_type = 0
-        left_sensor.field_of_view = 0.523599
-        left_sensor.min_range = 0.02
-        left_sensor.max_range = 1.00
+        left_sensor.field_of_view = 0.610865
+        left_sensor.min_range = 0.0
+        left_sensor.max_range = 1.01
         left_sensor.range = msg.data
         self.left.publish(left_sensor)        
     
     #############################################################################
     def rightCallback(self,msg):
     #############################################################################
+        usr = TransformBroadcaster()
+        usr.sendTransform((0.05 ,-0.085, -0.025),
+                          (0, 0, -0.258819, 0.9659258),
+                          rospy.Time.now(),
+                          "ultrasound_right",
+                          "base_link")
         right_sensor = Range()
         right_sensor.header.stamp = rospy.Time.now()
-        right_sensor.header.frame_id = "/ultrasound_right"
+        right_sensor.header.frame_id = "ultrasound_right"
         right_sensor.radiation_type = 0
-        right_sensor.field_of_view = 0.523599
-        right_sensor.min_range = 0.02
-        right_sensor.max_range = 1.00
+        right_sensor.field_of_view = 0.610865
+        right_sensor.min_range = 0.0
+        right_sensor.max_range = 1.01
         right_sensor.range = msg.data
         self.right.publish(right_sensor)
     
     #############################################################################
     def centerCallback(self,msg):
     #############################################################################
+        usc = TransformBroadcaster()
+        usc.sendTransform((0.06 ,0.0, -0.025),
+                          (0.0, 0.0, 0.0, 1.0),
+                          rospy.Time.now(),
+                          "ultrasound_center",
+                          "base_link")
         center_sensor = Range()
         center_sensor.header.stamp = rospy.Time.now()
-        center_sensor.header.frame_id = "/ultrasound_center"
+        center_sensor.header.frame_id = "ultrasound_center"
         center_sensor.radiation_type = 0
-        center_sensor.field_of_view = 0.523599
-        center_sensor.min_range = 0.02
-        center_sensor.max_range = 1.00
+        center_sensor.field_of_view = 0.610865
+        center_sensor.min_range = 0.0
+        center_sensor.max_range = 1.01
         center_sensor.range = msg.data
         self.center.publish(center_sensor)
         
