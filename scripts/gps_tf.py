d93ebadcc4332cd2c7e83775d30c7faf7c6f1e40
Varun Kumar T P <varunkumartp@gmail.com>
2021-06-29 17:15:15 +0530

    Removed else condition

--- scripts/gps_tf.py
+++ scripts/gps_tf.py
@@ -70,12 +70,11 @@ class gps_tf:
     #############################################################################
         if(self.serialPort.in_waiting > 0):
             a = self.serialPort.readline().decode('ascii', errors='replace')
+            lat = "0.00"
+            lon = "0.00"
             for line in a.split('\n') :
                 if line.startswith( '$GPGGA' ) :
                     lat, _, lon = line.strip().split(',')[2:5]
-                else:
-                    lat = '0.0'
-                    lon = '0.0'
                 self.latitude = round(float(lat)/100,6)
                 self.longitude = round(float(lon)/100,6)
         #rospy.loginfo("latitude = %f - longitude = %f",self.latitude, self.longitude)     
