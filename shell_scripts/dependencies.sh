d25f5dbfb520a1ef32f1ef2e45de017042475264
Varun Kumar T P <63554822+varun-kumar-tp@users.noreply.github.com>
2021-07-04 23:49:00 +0530

    Update dependencies.sh

--- shell_scripts/dependencies.sh
+++ shell_scripts/dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo apt-get install -y ros-noetic-rtabmap-ros
+sudo apt-get install -y ros-noetic-geographic-msgs
 sudo apt-get install -y libfreenect-dev
 sudo apt-get install -y libsdl-image1.2-dev
 sudo apt-get install -y libsdl-dev
