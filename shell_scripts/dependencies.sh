b9560bf2532fb15d16404fe5edf535010746d625
Varun Kumar T P <63554822+varun-kumar-tp@users.noreply.github.com>
2021-07-04 23:49:37 +0530

    Update dependencies.sh

--- shell_scripts/dependencies.sh
+++ shell_scripts/dependencies.sh
@@ -4,3 +4,4 @@ sudo apt-get install -y ros-noetic-geographic-msgs
 sudo apt-get install -y libfreenect-dev
 sudo apt-get install -y libsdl-image1.2-dev
 sudo apt-get install -y libsdl-dev
+sudo apt-get install -y libgeographic-dev
